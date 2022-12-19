b5ff3b5393cd718422cf7e74e5c8b74e2f8168ec
zabubu <samola801lounes@gmail.com>
2022-12-19 16:30:30 +0100

    ajout de la classe position

---
+++ Hunted_by_wild_animals_Game/Position.h
@@ -0,0 +1,55 @@
+#ifndef POSITION_H
+#define POSITION_H
+#include<cmath>
+
+class Position
+{
+    public:
+    /**
+            * @brief Constructor of joueur
+            *
+            * @param x : int
+            * @param y : int
+            * @return Create a position with x and y
+            */
+        Position(int x=0,int y=0);
+    /**
+            * @brief fonction of position
+            *
+            * @return  x
+            */
+        double renvoyerX()const;
+    /**
+            * @brief fonction of position
+            *
+            * @return  y
+            */
+        double renvoyerY()const;
+    /**
+            * @brief fonction of position
+            *
+            *   change x
+            */
+        void modifierX(double x);
+    /**
+            * @brief fonction of position
+            *
+            *   change x
+            */
+        void modifierY(double y);
+    /**
+            * @brief fonction of position
+            *
+            *   @param p : position
+            *
+            *   @return  distance
+            */
+        double renvoyerDistance(const Position& p)const;
+
+    private:
+    //The position of the joueur x:columns / y:rows
+        double d_x;
+        double d_y;
+};
+
+#endif // POSITION_H
