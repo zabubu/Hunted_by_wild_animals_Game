9e30e359d9c522ea956ef6b7ff88b30c034975fa
zabuza <samola801lounes@gmail.com>
2022-12-30 18:34:48 +0100

    modification de la classe tigre
    Co-Authored-By: KingYas68 <120946916+KingYas68@users.noreply.github.com>
    Co-Authored-By: Yacine <84641377+TahYacine@users.noreply.github.com>

--- Hunted_by_wild_animals_Game/tigre.cpp
+++ Hunted_by_wild_animals_Game/tigre.cpp
@@ -1,11 +1,184 @@
 #include "tigre.h"
-/*tigre::prochainePosition(const joueur &j)
+#include "position.h"
+#include "joueur.h"
+#include "arbres.h"
+#include "terrain.h"
+#include "pieges.h"
+#include "interface.h"
+
+tigre::tigre(const position & pos) : fauves{pos}
 {
-    position p{}=trouvePosition(j);
-    if(DeplacementAutorise(p))
+
+}
+
+position tigre::avanceVers(int i)
+{
+    if(i==8)
     {
-    deplaceEn(p);
+        position pos{posElem().i()-1, posElem().j()};
+        return pos;
+    }
+    if(i==2)
+    {
+       position pos{posElem().i()+1, posElem().j()};
+       return pos;
+    }
+    if(i==6)
+    {
+       position pos{posElem().i(), posElem().j()+1};
+       return pos;
+    }
+    if(i==4)
+    {
+       position pos{posElem().i(), posElem().j()-1};
+       return pos;
+    }
+    if(i==7)
+    {
+       position pos{posElem().i()-1, posElem().j()-1};
+       return pos;
+    }
+    if(i==9)
+    {
+       position pos{posElem().i()-1, posElem().j()+1};
+       return pos;
+    }
+    if(i==1)
+    {
+       position pos{posElem().i()+1, posElem().j()-1};
+       return pos;
+    }
+    if(i==3)
+    {
+       position pos{posElem().i()+1, posElem().j()+1};
+       return pos;
     }
 }
 
-*/
+
+
+int tigre::deplaceEn(const position &pos, terrain &t)
+{
+
+       int i = pos.i();
+       int j = pos.j();
+       auto elem = t.tabElements()[i][j].get();
+       if(dynamic_cast<tigre*>(elem))
+       {
+          return 1;
+       }
+       else if(dynamic_cast<fauves*>(elem))
+       {
+          return 2;
+       }
+       else if(dynamic_cast<joueur*>(elem))
+       {
+          return 2;
+       }
+        else if(dynamic_cast<pieges*>(elem))
+       {
+           return 3;
+       }
+        else if(dynamic_cast<arbres*>(elem))
+       {
+           return 1;
+       }
+       else if(elem==nullptr)
+       {
+           return 0;
+       }
+
+    }
+
+
+std::unique_ptr<elements> tigre::clone()
+{
+    auto p {std::make_unique<tigre>(posElem())};
+    return p;
+}
+
+void tigre::affiche(const interface &inter) const
+{
+     inter.afficheTigre(*this);
+}
+
+std::vector<int> tigre::directionDeplacement(const position &posJoueur) const
+{
+     std::vector<int> tabDeplacements;
+     int Ijoueur = posJoueur.i(), Jjoueur = posJoueur.j(), Ilion = d_pos.i(), Jlion = d_pos.j();
+
+
+     if(Jlion > Jjoueur)
+     {
+         if(Ilion < Ijoueur)
+         {
+            tabDeplacements.push_back(1);
+            tabDeplacements.push_back(4);
+            tabDeplacements.push_back(2);
+         }
+         else if(Ilion > Ijoueur)
+         {
+            tabDeplacements.push_back(7);
+            tabDeplacements.push_back(4);
+            tabDeplacements.push_back(8);
+         }
+         else
+         {
+             tabDeplacements.push_back(4);
+             tabDeplacements.push_back(7);
+             tabDeplacements.push_back(1);
+         }
+     }
+     else if(Jlion < Jjoueur)
+     {
+         if(Ilion < Ijoueur)
+         {
+            tabDeplacements.push_back(3);
+            tabDeplacements.push_back(6);
+            tabDeplacements.push_back(2);
+         }
+         else if(Ilion > Ijoueur)
+         {
+            tabDeplacements.push_back(9);
+            tabDeplacements.push_back(6);
+            tabDeplacements.push_back(8);
+         }
+         else
+         {
+             tabDeplacements.push_back(6);
+             tabDeplacements.push_back(3);
+             tabDeplacements.push_back(9);
+         }
+     }
+     else if(Jlion == Jjoueur)
+     {
+         if(Ilion > Ijoueur)
+         {
+            tabDeplacements.push_back(8);
+            tabDeplacements.push_back(7);
+            tabDeplacements.push_back(9);
+         }
+         else if(Ilion < Ijoueur)
+         {
+            tabDeplacements.push_back(2);
+            tabDeplacements.push_back(1);
+            tabDeplacements.push_back(3);
+         }
+     }
+     else if(Ilion == Ijoueur)
+     {
+         if(Jlion > Jjoueur)
+         {
+            tabDeplacements.push_back(4);
+            tabDeplacements.push_back(7);
+            tabDeplacements.push_back(1);
+         }
+         else if(Jlion < Jjoueur)
+         {
+            tabDeplacements.push_back(6);
+            tabDeplacements.push_back(9);
+            tabDeplacements.push_back(3);
+         }
+     }
+          return tabDeplacements;
+}
--- Hunted_by_wild_animals_Game/tigre.h
+++ Hunted_by_wild_animals_Game/tigre.h
@@ -1,14 +1,29 @@
 #ifndef TIGRE_H
 #define TIGRE_H
+
+
 #include "fauves.h"
+#include <iostream>
+
+
+class position;
+class terrain;
+class interface;
 
 class tigre : public fauves
 {
     public :
-    /***
-     * @brief Change the position of the lion to make him reach the player
-     * @param j : Player
-    */
-    /*void deplaceVers(const joueur &j) override;*/
+    tigre(const position &pos);
+    position avanceVers(int i) override;
+    int deplaceEn(const position &pos, terrain &t) override;
+    std::unique_ptr<elements> clone() override;
+    void affiche(const interface &inter) const override;
+    std::vector<int> directionDeplacement(const position &posJoueur) const override;
+
 };
-#endif
+
+
+
+#endif // TIGRE_H
+
+
