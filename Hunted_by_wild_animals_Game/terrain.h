3275ed97a7321a787cc9c4dba6714399206463bd
KingYas68 <psgya68@gmail.com>
2022-12-30 20:18:46 +0100

    mise a jour de la classe terrain

--- Hunted_by_wild_animals_Game/terrain.h
+++ Hunted_by_wild_animals_Game/terrain.h
@@ -1,57 +1,11 @@
-#ifndef TERRAIN_H
-#define TERRAIN_H
-
-#include <memory>
-#include <vector>
-
-#include <fstream>
-
-class interface;
-class elements;
-class position;
-
-
-
-class terrain
-{
-public :
-    terrain();
-    terrain(int hauteur, int largeur, int nbElem, int nbFauves, int nbPieges);
-    terrain(const terrain &t);
-
-    void taille(int hauteur, int largeur);
-
-    double nombreElements() const;
-    double nombreFauves() const;
-    double nombrePieges() const;
-    double hauteur() const;
-    double largeur() const;
-    const std::vector<std::vector<std::unique_ptr<elements>>>& tabElements() const;
-    std::vector<position> tabPosFauves() const;
-    position posJoueur() const;
-
-    void ajouterElement(std::unique_ptr<elements> elem, const position &p);
-    void supprimerElement(const position &p);
-
-    void lireDepuisFichier(std::istream& ist);
-    void enregistreDansFichier(std::ostream& ost);
-    void creeTerrain();
-    void affiche(const interface & inter) const;
-
-    void modifierNbElements(int elem);
-    void modifierNbFauves(int fauves);
-    void modifierNbPieges(int pieges);
-
-private :
-    int d_nombreElements;
-    int d_nombreFauves;
-    int d_nombrePieges;
-    int d_hauteur;
-    int d_largeur;
-    std::vector<std::vector<std::unique_ptr<elements>>> d_elems;
-};
-
-
-
-#endif // TERRAIN_H
-
+[10;10]
+P..L......
+.P........
+..P...L...
+...P....T.
+.J..P.....
+..........
+.P........
+..L...T...
+...T......
+....A.....
