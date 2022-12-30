cdaa1d5143c6879438384b4366afd2924acd370c
zabuza <samola801lounes@gmail.com>
2022-12-30 18:28:51 +0100

    Modification de la classe Fauve
    Co-Authored-By: Yacine <84641377+TahYacine@users.noreply.github.com>

--- Hunted_by_wild_animals_Game/fauves.cpp
+++ Hunted_by_wild_animals_Game/fauves.cpp
@@ -1,33 +1,7 @@
 #include "fauves.h"
+#include "position.h"
 
-fauves::fauves(const std::string& type) : elements{type}
-{}
-
-fauves::fauves(const position &p,const std::string& type) : elements{p,type}
-{}
-
-fauves::fauves(int x, int y,const std::string& type) : elements{x,y,type}
-{}
-
-bool fauves::deplacementAutorise(const position &p)const
+fauves::fauves(const position & pos) : elementsDeplacable{pos}
 {
-/*    if(p.possible())
-    {
-        if(terrain.libre(p))
-        {
-            return true;
-        }
-    }
-*/
-}
-/*
-void fauves::deplaceVers(const joueur &j)
-{
-    position p=prochainePosition(j);
-    if(DeplacementAutorise(p))
-    {
-    changePosition(p);
-    }
-}
 
-*/
+}
--- Hunted_by_wild_animals_Game/fauves.h
+++ Hunted_by_wild_animals_Game/fauves.h
@@ -1,41 +1,30 @@
 #ifndef FAUVES_H
 #define FAUVES_H
-#include "position.h"
-#include "elements.h"
-#include "joueur.h"
 
-class fauves : public elements{
-    public:
-    /**
-     * @brief Empty constructor of the class fauves
-     * @param Name of the type of the fauves
-     * @return Creation of an element of the class fauves
-    */
-    fauves(const std::string& type);
-    /**
-     * @brief position constructor of the class fauves
-     * @param p : position
-     * @return Creation of an element of the class fauves with the coordinates of p
-    */
-    fauves(const position &p,const std::string& type);
-    /**
-     * @brief Coordinates constructor of the class fauves
-     * @param x : int
-     * @param y : int
-     * @return Creation of an element of the class fauves with the coordinates of (x,y)
-    */
-    fauves(int x, int y,const std::string& type);
-    /**
-     * @brief Change the position of the "fauves" if it's possible
-     * @param j : Player where the "fauves" have to go
-    */
-    void deplaceVers(const joueur &j);
-    /**
-     * @brief Permission to move the "fauves"
-     * @param p : position where the fauves need to go
-     * @return True if the position is empty, false if a tree is on the position
-    */
-    bool deplacementAutorise(const position& p)const;
+#include "elementsDeplacable.h"
+#include <string>
+#include <iostream>
+#include <vector>
+
+
+class position;
+class elements;
+class interface;
+
+
+class fauves : public elementsDeplacable
+{
+public :
+    fauves(const position & pos);
+    position avanceVers(int i) override =0;
+    int deplaceEn(const position &pos, terrain &t) override = 0;
+    std::unique_ptr<elements> clone() override =0;
+    void affiche(const interface &inter) const override=0;
+    virtual std::vector<int> directionDeplacement(const position &posJoueur) const=0;
+
 };
 
-#endif
+
+
+
+#endif // FAUVES_H
