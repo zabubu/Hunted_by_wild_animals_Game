070897aa791a157adf1ccefd293fec876e6ee8b3
TahYacine <yacine.tahiry@gmail.com>
2022-12-31 14:08:01 +0100

    Modifications des tests de tigre

--- Test_Hunted_by_wild_animals_Game/Testtigre.cpp
+++ Test_Hunted_by_wild_animals_Game/Testtigre.cpp
@@ -1,32 +1,80 @@
 #ifndef TESTTIGRE_H
 #define TESTTIGRE_H
-
 #include "doctest.h"
 #include <sstream>
 #include <string>
-#include "../Hunted_by_wild_animals_Game/position.h"
+#include "../Hunted_by_wild_animals_Game/tigre.h"
 
 using std::ostringstream;
 using std::string;
 
-TESTCASE("Les opérations sur les tigres sont corrects")
+constexpr int enHaut=8,aDroite=6,aGauche=4,enBas=2;
+
+TEST_CASE("Les opérations sur les positions des tigres sont corrects")
 {
-	SUBCASE("Deplacer un tigre vers un joueur est correct")
-	{
-		SUBCASE("Deplacer un tigre vers un joueur au dessus de lui")
-		{
-			joueur jou{1,5};
-			tigre tig{1,1};
-			tig.deplaceVers(jou);
-			REQUIRE_EQ((tig.position(),p{2,2})||tig.position(),p{0,2});
-		}
-				SUBCASE("Deplacer un tigre vers un joueur au dessus de lui")
-		{
-			joueur jou{1,1};
-			tigre tig{1,5};
-			tig.deplaceVers(jou);
-			REQUIRE_EQ((tig.position(),p{2,4})||tig.position(),p{0,4});
-		}
-	}
+    SUB_CASE("Creer un tigre à une position donnée est correct")
+    {
+        position pos(3,4);
+        tigre tig(pos);
+        REQUIRE_EQ(tig.posElem(),pos);
+    }
+    TEST_CASE("Changer la position d'un tigre est correct")
+    {
+            position pos(5,5);
+            tigre tig(pos);
+        SUB_CASE("Avancer un tigre vers le haut est correct")
+        {;
+            pos.modifieI(4);
+            REQUIRE_EQ(tig.avanceVers(enHaut),pos);
+        }
+        SUB_CASE("Avancer un tigre vers la droite est correct")
+        {
+            pos.modifieJ(6);
+            REQUIRE_EQ(tig.avanceVers(aDroite),pos);
+        }
+        SUB_CASE("Avancer un tigre vers la gauche est correct")
+        {
+            pos.modifieJ(4);
+            REQUIRE_EQ(tig.avanceVers(aGauche),pos);
+        }
+        SUB_CASE("Avancer un tigre vers le bas est correct")
+        {
+            pos.modifieI(6);
+            REQUIRE_EQ(tig.avanceVers(enBas),pos);
+        }
+        SUB_CASE("Avancer un tigre vers le haut droite est correct")
+        {;
+            pos.modifieI(4);
+            pos.modifieJ(6);
+            REQUIRE_EQ(tig.avanceVers(9),pos);
+        }
+        SUB_CASE("Avancer un tigre vers le bas droite est correct")
+        {
+            pos.modifieI(4);
+            pos.modifieJ(4);
+            REQUIRE_EQ(tig.avanceVers(3),pos);
+        }
+        SUB_CASE("Avancer un tigre vers le bas gauche est correct")
+        {
+            pos.modifieI(6);
+            pos.modifieJ(4);
+            REQUIRE_EQ(tig.avanceVers(1),pos);
+        }
+        SUB_CASE("Avancer un tigre vers le haut gauche est correct")
+        {
+            pos.modifieI(6);
+            pos.modifieJ(6);
+            REQUIRE_EQ(tig.avanceVers(7),pos);
+        }
+    }
+    TEST_CASE("Deplacer un tigre vers un joueur est correct")
+    {
+        SUB_CASE("Deplacer un tigre vers un joueur au dessus est correct")
+        {
+
+        }
+    }
 }
+
+
 #endif
