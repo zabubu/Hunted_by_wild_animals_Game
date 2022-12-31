c37bd17185813f4ae2d27aef8f460e59571596a3
Heuss_Hcn <83731767+HHasashi@users.noreply.github.com>
2022-12-31 02:02:13 +0100

    Tests arbres et pièges
    ajout des tests pour arbres et pièges

---
+++ Test_Hunted_by_wild_animals_Game/Testabres.cpp
@@ -0,0 +1,29 @@
+#ifndef TESTARBRES_H
+#define TESTARBRES_H
+
+#include "doctest.h"
+#include <sstream>
+#include <string>
+#include "../Hunted_by_wild_animals_Game/arbres.h"
+
+using std::ostringstream;
+using std::string;
+
+TEST_CASE("Construction de l'arbre")
+{
+        SUB_CASE("L'arbre est construit à un point donné")
+        {
+            arbres a{position{20,15}};
+            REQUIRE_EQ(a.posElem().i(), 20);
+            REQUIRE_EQ(a.posElem().j(), 20);
+        }
+
+        SUB_CASE("L'arbre est construit à (0;0)")
+        {
+            arbres a{};
+            REQUIRE_EQ(a.posElem().i(), 0);
+            REQUIRE_EQ(a.posElem().j(), 0);
+        }
+}
+
+#endif
