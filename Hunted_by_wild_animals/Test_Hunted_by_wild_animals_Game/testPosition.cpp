f40052d2664901d4a37cd494b16bba568b467e6a
zabuza <samola801lounes@gmail.com>
2023-01-01 20:08:46 +0100

    modification des tests position

--- Hunted_by_wild_animals/Test_Hunted_by_wild_animals_Game/testPosition.cpp
+++ Hunted_by_wild_animals/Test_Hunted_by_wild_animals_Game/testPosition.cpp
@@ -4,15 +4,46 @@
 
 TEST_CASE ("Test de la classe position")
 {
+    int i=2,j=4;
     SUBCASE("Test du constructeur")
     {
-        int i=2,j=4;
-        position p{i,j};
-
         SUBCASE("la position est correcte")
         {
+            position p{i,j};
             REQUIRE_EQ(p.getI(),i);
             REQUIRE_EQ(p.getJ(),j);
         }
+        SUBCASE("Test du constructeur par defaut")
+        {
+            position p{};
+            REQUIRE_EQ(p.getI(),0);
+            REQUIRE_EQ(p.getJ(),0);
+        }
+    }
+    SUBCASE("Test des fonctions set")
+    {
+        position p{};
+        p.setI(i);
+        p.setJ(j);
+        REQUIRE_EQ(p.getI(),i);
+    	REQUIRE_EQ(p.getJ(),j);
+    }
+    SUBCASE("Test de l'operator =")
+    {
+        position p{i,j};
+        position pT = p;
+        REQUIRE_EQ(p, pT);
+    }
+    SUBCASE("Test de l'operator ==")
+    {
+        position p{i,j};
+        position pT{i,j};
+        REQUIRE_EQ(p, pT);
+    }
+    SUBCASE("Test de l'operator !=")
+    {
+        position p{i,j};
+        position pT{i+1,j+1};
+        REQUIRE_NE(pT, p);
     }
 }
