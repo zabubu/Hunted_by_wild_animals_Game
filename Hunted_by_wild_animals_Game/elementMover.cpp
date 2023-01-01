2f663c1f5610eb873583acf8616f7fa08b09d165
KingYas68 <psgya68@gmail.com>
2023-01-01 13:09:27 +0100

    Mise a jour de la classe elementMover

--- Hunted_by_wild_animals_Game/elementMover.cpp
+++ Hunted_by_wild_animals_Game/elementMover.cpp
@@ -1,1 +1,18 @@
+#include "elementMover.h"
+#include "terrain.h"
+
+elementMover::elementMover(int direction, const position &posElement) : d_direction{direction}, d_posElement{posElement}
+{
+
+}
+
+int elementMover::direction() const
+{
+    return d_direction;
+}
+
+position elementMover::positionElem() const
+{
+    return d_posElement;
+}
 
--- Hunted_by_wild_animals_Game/elementMover.h
+++ Hunted_by_wild_animals_Game/elementMover.h
@@ -1,1 +1,27 @@
+#ifndef ELEMENTMOVER_H
+#define ELEMENTMOVER_H
+#include "position.h"
+class terrain;
 
+class elementMover
+{
+public :
+    elementMover(int direction, const position &posElement);
+    ~elementMover() = default;
+    virtual position nouvellePositionPotentiel()=0;
+    virtual void deplace(terrain &t) = 0;
+    int direction() const;
+    position positionElem() const;
+    virtual int situation(terrain &t)=0;
+    virtual position nouvellePosition(terrain &t) =0;
+
+    const int SITUATION_PEUT_SE_DEPLACER {1};
+    const int SITUATION_NE_PEUT_SE_DEPLACER {2};
+    const int SITUATION_MORT {3};
+
+protected:
+    int d_direction;
+    position d_posElement;
+
+};
+#endif // ELEMENTMOVER_H
