c40ee95da46a450941233d85cf519dfe9cb932f8
KingYas68 <psgya68@gmail.com>
2023-01-01 13:07:52 +0100

    Mise a jour de la classe fauve
    Co-Authored-By: Heuss_Hcn <83731767+HHasashi@users.noreply.github.com>

--- Hunted_by_wild_animals_Game/fauves.h
+++ Hunted_by_wild_animals_Game/fauves.h
@@ -16,11 +16,9 @@ class fauves : public elementsDeplacable
 {
 public :
     fauves(const position & pos);
-    position avanceVers(int i) override =0;
-    int deplaceEn(const position &pos, terrain &t) override = 0;
     std::unique_ptr<elements> clone() override =0;
     void affiche(const interface &inter) const override=0;
-    virtual std::vector<int> directionDeplacement(const position &posJoueur) const=0;
+    void deplace(terrain &t , int direction) override =0;
 
 };
 
