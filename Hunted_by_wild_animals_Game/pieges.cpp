2658dda8e297d7f49ac18b82f8ba9c59b7f0112d
KingYas68 <psgya68@gmail.com>
2023-01-01 13:04:37 +0100

    Mise a jour de la classe Piege
    Co-Authored-By: Yacine <84641377+TahYacine@users.noreply.github.com>

--- Hunted_by_wild_animals_Game/pieges.cpp
+++ Hunted_by_wild_animals_Game/pieges.cpp
@@ -1,20 +1,65 @@
-#include "pieges.h"
 #include "position.h"
-#include "interface.h"
-
-
-pieges::pieges(const position & pos) : elements{pos}
+position::position() : d_i{0}, d_j{0}
 {
 
 }
 
-std::unique_ptr<elements> pieges::clone()
+position::position(int i, int j) : d_i{i}, d_j{j}
 {
 
 }
 
-void pieges::affiche(const interface &inter) const
+
+position::position(const position &p) : d_i{p.i()}, d_j{p.j()}
 {
-    inter.affichePieges(*this);
+
 }
+
+int position::i() const
+{
+    return d_i;
+}
+
+int position::j() const
+{
+    return d_j;
+}
+
+void position::modifieI(int i)
+{
+    d_i=i;
+}
+
+void position::modifieJ(int j)
+{
+    d_j = j;
+}
+
+void position::affiche(std::ostream &ost) const
+{
+    ost<<"["<<d_i<<";"<<d_j<<"]";
+}
+
+position& position::operator=(const position& p)
+{
+    if(this != &p)
+    {
+        d_i = p.i();
+        d_j = p.j();
+    }
+    return *this;
+}
+
+bool position::operator!=(const position& p) const
+{
+    return (d_i != p.i()) || (d_j != p.j());
+}
+
+bool position::operator==(const position& p) const
+{
+    return (d_i == p.i()) && (d_j == p.j());
+}
+
+
+
 
--- Hunted_by_wild_animals_Game/pieges.h
+++ Hunted_by_wild_animals_Game/pieges.h
@@ -1,22 +1,27 @@
-#ifndef PIEGES_H
-#define PIEGES_H
+#ifndef POSITION_H
+#define POSITION_H
 
-#include <iostream>
-#include "elements.h"
-#include <memory>
+#include <fstream>
 
-class interface;
-class position;
-
-class pieges : public elements
+class position
 {
-public :
-    pieges(const position & pos);
-    std::unique_ptr<elements> clone() override;
-    void affiche(const interface &inter) const override;
+public:
+    position();
+    position(int i, int j);
+    position (const position &p);
+    int i() const;
+    int j() const;
+    void modifieI(int i);
+    void modifieJ(int j);
+    void affiche(std::ostream &ost) const;
+      position& operator=(const position& p);
+      bool operator!=(const position& p) const;
+      bool operator==(const position& p) const;
+
+private:
+    int d_i, d_j;
 };
 
 
 
-
-#endif // PIEGES_H
+#endif // POSITION_H
